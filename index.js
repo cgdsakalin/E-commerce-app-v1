b31f251265da60e83856c01773edf6e931b97d7a
cgdsakalin <cgdsakalin@gmail.com>
2022-12-07 17:32:54 +0300

    Setting up express server

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+app.listen(3000, function () {
+  console.log("server listening on port 3000");
+});
